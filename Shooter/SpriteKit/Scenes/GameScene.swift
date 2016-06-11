e175788cd8706f2ddd5b2431d34f6fc2bdb5337d
Eric Internicola <intere@gmail.com>
2016-06-10 22:01:09 -0600

    Added Screenshots, and apparently some other code.

--- Shooter/SpriteKit/Scenes/GameScene.swift
+++ Shooter/SpriteKit/Scenes/GameScene.swift
@@ -66,10 +66,8 @@ extension GameScene {
     }
 }
 
+// MARK: - SKPhysicsContactDelegate Methods
 extension GameScene : SKPhysicsContactDelegate {
-
-    // MARK: - SKPhysicsContactDelegate Methods
-    
     func didBeginContact(contact: SKPhysicsContact) {
         let firstBody = contact.bodyA
         let secondBody = contact.bodyB
@@ -88,25 +86,183 @@ extension GameScene : SKPhysicsContactDelegate {
             }
         }
     }
-    
-}
 
-// MARK: - Helper Methods
-extension GameScene {
     func enemyPlayerCollision(enemyTemp: SKSpriteNode, playerTemp: SKSpriteNode) {
         runAction(SoundProvider.instance.dalekExterminate)
         if let mainLabel = mainLabel, player = player {
             mainLabel.fontSize = 50
             mainLabel.alpha = 1.0
             mainLabel.text = "Game Over"
-            
+
             player.removeFromParent()
             isAlive = false
         }
-        
+
         waitThenMoveToTitleScreen()
     }
-    
+
+    func projectileCollision(enemyTemp: SKSpriteNode, projectileTemp: SKSpriteNode) {
+        enemyTemp.removeFromParent()
+        projectileTemp.removeFromParent()
+        score = score + 1
+        updateScore()
+    }
+}
+
+// MARK: - Spawning Methods
+extension GameScene {
+    func spawnBackground() {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            if view?.frame.height > view?.frame.width {
+                background = SKSpriteNode(texture: TextureProvider.instance.background1iPadPortrait)
+                background?.xScale = 1.2
+                background?.yScale = 1.2
+            } else {
+                background = SKSpriteNode(texture: TextureProvider.instance.background1iPadLandscape)
+                background?.xScale = 1.5
+                background?.yScale = 1.5
+            }
+        } else {
+            background = SKSpriteNode(texture: TextureProvider.instance.background1)
+            background?.xScale = 1.2
+            background?.yScale = 1.2
+        }
+        if let background = background {
+            background.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
+            background.zPosition = -10
+            addChild(background)
+        }
+    }
+
+    func spawnPlayer() {
+        player = SKSpriteNode(texture: TextureProvider.instance.tardisTexture)
+        if let player = player {
+            player.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMinY(frame) + 100)
+            player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
+            player.physicsBody?.affectedByGravity = false
+            player.physicsBody?.categoryBitMask = PhysicsCategory.player
+            player.physicsBody?.contactTestBitMask = PhysicsCategory.enemy
+            player.physicsBody?.dynamic = false
+
+            addChild(player)
+        }
+    }
+
+    func spawnEnemy() {
+        guard isAlive else {
+            return
+        }
+
+        enemy = SKSpriteNode(texture: TextureProvider.instance.dalekTexture)
+        if let enemy = enemy {
+
+            let maxX = CGRectGetMaxX(frame)
+            let maxY = CGRectGetMaxY(frame)
+            let randomX = random() * (CGRectGetMaxX(frame) - 2 * enemy.frame.size.width) + enemy.frame.size.width
+            enemy.position = CGPoint(x: randomX, y: CGRectGetMaxY(frame) + 300)
+
+            enemy.physicsBody = SKPhysicsBody(rectangleOfSize: enemy.size)
+            enemy.physicsBody?.affectedByGravity = false
+            enemy.physicsBody?.categoryBitMask = PhysicsCategory.enemy
+            enemy.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
+            enemy.physicsBody?.allowsRotation = false
+            enemy.physicsBody?.dynamic = true
+
+            let moveForward = SKAction.moveToY(-100, duration: enemySpeed)
+            let destroy = SKAction.removeFromParent()
+
+            enemy.runAction(SKAction.sequence([moveForward, destroy]))
+
+            addChild(enemy)
+
+            print("Created Dalek at X: \(enemy.position.x) in size: \(maxX)x\(maxY)")
+        }
+    }
+
+    func spawnExplosion(enemyTemp: SKSpriteNode) {
+        if let explosionEmitterPath = NSBundle.mainBundle().pathForResource("explosion", ofType: "sks"), explosion = NSKeyedUnarchiver.unarchiveObjectWithFile(explosionEmitterPath) as? SKEmitterNode {
+            explosion.position = CGPoint(x: enemyTemp.position.x, y: enemyTemp.position.y)
+            explosion.zPosition = 1
+            explosion.targetNode = self
+            addChild(explosion)
+            let explosionTimerRemove = SKAction.waitForDuration(0.5)
+            let removeExplosion = SKAction.runBlock {
+                explosion.removeFromParent()
+            }
+            runAction(SKAction.sequence([explosionTimerRemove, removeExplosion]))
+        } else {
+            print("ERROR: Creating explosion")
+        }
+    }
+
+    func spawnScoreLabel() {
+        scoreLabel = SKLabelNode(fontNamed: "Futura")
+        if let scoreLabel = scoreLabel {
+            scoreLabel.fontSize = 50
+            scoreLabel.fontColor = textColorHUD
+            scoreLabel.position = CGPoint(x: CGRectGetMidX(frame), y: 5)
+            scoreLabel.text = "Score"
+            addChild(scoreLabel)
+        }
+    }
+
+    func spawnMainLabel() {
+        mainLabel = SKLabelNode(fontNamed: "Futura")
+        if let mainLabel = mainLabel {
+            mainLabel.fontSize = 100
+            mainLabel.fontColor = textColorHUD
+            mainLabel.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
+            mainLabel.text = "Start"
+
+            addChild(mainLabel)
+        }
+    }
+
+    func spawnProjectile() {
+        projectile = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 3, height: 15))
+        if let projectile = projectile, player = player {
+            projectile.position = CGPoint(x: player.position.x, y: player.position.y)
+            projectile.physicsBody = SKPhysicsBody(rectangleOfSize: projectile.size)
+            projectile.physicsBody?.affectedByGravity = false
+            projectile.physicsBody?.categoryBitMask = PhysicsCategory.projectile
+            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.enemy
+            projectile.physicsBody?.dynamic = false
+            projectile.zPosition = -1
+
+            addChild(projectile)
+
+            let moveForward = SKAction.moveToY(CGRectGetMaxY(frame), duration: projectileSpeed)
+            let destroy = SKAction.removeFromParent()
+
+            projectile.runAction(SKAction.sequence([moveForward, destroy]))
+        }
+    }
+}
+
+// MARK: - Spawn Timers
+extension GameScene {
+    func fireProjectile() {
+        let fireProjectileTimer = SKAction.waitForDuration(fireProjectileRate)
+        let spawn = SKAction.runBlock {
+            self.spawnProjectile()
+        }
+        let sequence = SKAction.sequence([fireProjectileTimer, spawn])
+        runAction(SKAction.repeatActionForever(sequence))
+    }
+
+    func randomEnemyTimerSpawn() {
+        let spawnEnemyTimer = SKAction.waitForDuration(enemySpawnRate)
+        let spawn = SKAction.runBlock {
+            self.spawnEnemy()
+        }
+        let sequence = SKAction.sequence([spawnEnemyTimer, spawn])
+        runAction(SKAction.repeatActionForever(sequence))
+    }
+}
+
+
+// MARK: - Helper Methods
+extension GameScene {
     func waitThenMoveToTitleScreen() {
         let wait = SKAction.waitForDuration(3)
         let transition = SKAction.runBlock {
@@ -120,31 +276,6 @@ extension GameScene {
         }
         let sequence = SKAction.sequence([wait, transition])
         runAction(SKAction.repeatAction(sequence, count: 1))
-    }
-    
-    func projectileCollision(enemyTemp: SKSpriteNode, projectileTemp: SKSpriteNode) {
-        enemyTemp.removeFromParent()
-        projectileTemp.removeFromParent()
-        score = score + 1
-        updateScore()
-    }
-    
-    func fireProjectile() {
-        let fireProjectileTimer = SKAction.waitForDuration(fireProjectileRate)
-        let spawn = SKAction.runBlock {
-            self.spawnProjectile()
-        }
-        let sequence = SKAction.sequence([fireProjectileTimer, spawn])
-        runAction(SKAction.repeatActionForever(sequence))
-    }
-    
-    func randomEnemyTimerSpawn() {
-        let spawnEnemyTimer = SKAction.waitForDuration(enemySpawnRate)
-        let spawn = SKAction.runBlock {
-            self.spawnEnemy()
-        }
-        let sequence = SKAction.sequence([spawnEnemyTimer, spawn])
-        runAction(SKAction.repeatActionForever(sequence))
     }
     
     func updateScore() {
@@ -168,134 +299,10 @@ extension GameScene {
         isAlive = true
         score = 0
     }
-    
-    func spawnBackground() {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            if view?.frame.height > view?.frame.width {
-                background = SKSpriteNode(texture: TextureProvider.instance.background1iPadPortrait)
-                background?.xScale = 1.2
-                background?.yScale = 1.2
-            } else {
-                background = SKSpriteNode(texture: TextureProvider.instance.background1iPadLandscape)
-                background?.xScale = 1.5
-                background?.yScale = 1.5
-            }
-        } else {
-            background = SKSpriteNode(texture: TextureProvider.instance.background1)
-            background?.xScale = 1.2
-            background?.yScale = 1.2
-        }
-        if let background = background {
-            background.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
-            background.zPosition = -10
-            addChild(background)
-        }
-    }
-    
-    func spawnPlayer() {
-        player = SKSpriteNode(texture: TextureProvider.instance.tardisTexture)
-        if let player = player {
-            player.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMinY(frame) + 100)
-            player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
-            player.physicsBody?.affectedByGravity = false
-            player.physicsBody?.categoryBitMask = PhysicsCategory.player
-            player.physicsBody?.contactTestBitMask = PhysicsCategory.enemy
-            player.physicsBody?.dynamic = false
-            
-            addChild(player)
-        }
-    }
-    
-    func spawnEnemy() {
-        guard isAlive else {
-            return
-        }
-        
-        enemy = SKSpriteNode(texture: TextureProvider.instance.dalekTexture)
-        if let enemy = enemy {
-            
-            let maxX = CGRectGetMaxX(frame)
-            let maxY = CGRectGetMaxY(frame)
-            let randomX = random() * (CGRectGetMaxX(frame) - 2 * enemy.frame.size.width) + enemy.frame.size.width
-            enemy.position = CGPoint(x: randomX, y: CGRectGetMaxY(frame) + 300)
-            
-            enemy.physicsBody = SKPhysicsBody(rectangleOfSize: enemy.size)
-            enemy.physicsBody?.affectedByGravity = false
-            enemy.physicsBody?.categoryBitMask = PhysicsCategory.enemy
-            enemy.physicsBody?.contactTestBitMask = PhysicsCategory.projectile
-            enemy.physicsBody?.allowsRotation = false
-            enemy.physicsBody?.dynamic = true
-            
-            let moveForward = SKAction.moveToY(-100, duration: enemySpeed)
-            let destroy = SKAction.removeFromParent()
-            
-            enemy.runAction(SKAction.sequence([moveForward, destroy]))
-            
-            addChild(enemy)
-            
-            print("Created Dalek at X: \(enemy.position.x) in size: \(maxX)x\(maxY)")
-        }
-    }
-    
-    func spawnExplosion(enemyTemp: SKSpriteNode) {
-        if let explosionEmitterPath = NSBundle.mainBundle().pathForResource("explosion", ofType: "sks"), explosion = NSKeyedUnarchiver.unarchiveObjectWithFile(explosionEmitterPath) as? SKEmitterNode {
-            explosion.position = CGPoint(x: enemyTemp.position.x, y: enemyTemp.position.y)
-            explosion.zPosition = 1
-            explosion.targetNode = self
-            addChild(explosion)
-            let explosionTimerRemove = SKAction.waitForDuration(0.5)
-            let removeExplosion = SKAction.runBlock {
-                explosion.removeFromParent()
-            }
-            runAction(SKAction.sequence([explosionTimerRemove, removeExplosion]))
-        } else {
-            print("ERROR: Creating explosion")
-        }
-    }
-    
-    func spawnScoreLabel() {
-        scoreLabel = SKLabelNode(fontNamed: "Futura")
-        if let scoreLabel = scoreLabel {
-            scoreLabel.fontSize = 50
-            scoreLabel.fontColor = textColorHUD
-            scoreLabel.position = CGPoint(x: CGRectGetMidX(frame), y: 5)
-            scoreLabel.text = "Score"
-            addChild(scoreLabel)
-        }
-    }
-    
-    func spawnMainLabel() {
-        mainLabel = SKLabelNode(fontNamed: "Futura")
-        if let mainLabel = mainLabel {
-            mainLabel.fontSize = 100
-            mainLabel.fontColor = textColorHUD
-            mainLabel.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame))
-            mainLabel.text = "Start"
-            
-            addChild(mainLabel)
-        }
-    }
-    
-    func spawnProjectile() {
-        projectile = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 3, height: 15))
-        if let projectile = projectile, player = player {
-            projectile.position = CGPoint(x: player.position.x, y: player.position.y)
-            projectile.physicsBody = SKPhysicsBody(rectangleOfSize: projectile.size)
-            projectile.physicsBody?.affectedByGravity = false
-            projectile.physicsBody?.categoryBitMask = PhysicsCategory.projectile
-            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.enemy
-            projectile.physicsBody?.dynamic = false
-            projectile.zPosition = -1
-            
-            addChild(projectile)
-            
-            let moveForward = SKAction.moveToY(CGRectGetMaxY(frame), duration: projectileSpeed)
-            let destroy = SKAction.removeFromParent()
-            
-            projectile.runAction(SKAction.sequence([moveForward, destroy]))
-        }
-    }
-    
+}
+
+// MARK: - Structures
+extension GameScene {
     struct PhysicsCategory {
         static let player = UInt32(1 << 0)
         static let enemy = UInt32(1 << 1)
